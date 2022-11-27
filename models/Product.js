51e45d0e69bb19a2d4b0a0bda9643c9c9dee316a
Nam Bui <nam.bui17@icloud.com>
2022-11-27 16:43:49 -0600

    All routes for api

--- models/Product.js
+++ models/Product.js
@@ -23,10 +23,17 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
       defaultValue: 10,
       validate: {
         isNumeric: true,
-      }
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
